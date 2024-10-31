34fe3edf5de2b23be963880a845847d73e997316
yassine7h <110841721+yassine7h@users.noreply.github.com>
2024-10-31 13:46:37 +0100

    init code

---
+++ src/main/java/dp/grp4/SecondaryController.java
@@ -0,0 +1,12 @@
+package dp.grp4;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
